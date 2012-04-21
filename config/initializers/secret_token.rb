9ac49c91df6e6fc8a6a2b742f6c390d27c9156ef
Nelson Wittwer <nelsonwittwer@gmail.com>
2012-04-21 13:17:44 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railshero::Application.config.secret_token = '3f7576ace4510d52979403200df63213e52cccd2712c38ee5f2d21aafedf321fd776dc6ea22216c295b2f1f716a4edcdf531e98334cd00c1dad09e2e58f86e82'
