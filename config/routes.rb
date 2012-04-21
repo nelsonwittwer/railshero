19a223530782a10b7b3c309263dfcf76ea4d6e2d
Nelson Wittwer <nelsonwittwer@gmail.com>
2012-04-21 13:25:58 -0600

    Added static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Railshero::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/FAQ"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
